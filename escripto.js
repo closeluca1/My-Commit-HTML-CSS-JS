fd199c5c6c7c60dd11f977e9e2a176ad40b448c8
Patrick Lucas <57973233+closeluca1@users.noreply.github.com>
2020-01-22 12:48:18 -0300

    Add files via upload

---
+++ escripto.js
@@ -0,0 +1,75 @@
+var tempo = 2000;
+
+function action(){
+	document.getElementById("a1").style.background = "black";
+	setTimeout(change, tempo);
+	
+};
+function change(){
+	document.getElementById("a1").style.background = "orange";
+	
+};
+
+function area2(){
+	document.getElementById("a2").style.background = "black";
+	setTimeout(change2, tempo);
+};
+function change2(){
+	document.getElementById("a2").style.background = "orange";
+};
+
+function area3(){
+	document.getElementById("a3").style.background = "black";
+	setTimeout(change3, tempo);
+};
+function change3(){
+	document.getElementById("a3").style.background = "orange";
+};
+
+function area4(){
+	document.getElementById("a4").style.background = "black";
+	setTimeout(change4, tempo);
+};
+function change4(){
+	document.getElementById("a4").style.background = "orange";
+};
+
+function area5(){
+	document.getElementById("a5").style.background = "black";
+	setTimeout(change5, tempo);
+};
+function change5(){
+	document.getElementById("a5").style.background = "orange";
+};
+
+function area6(){
+	document.getElementById("a6").style.background = "black";
+	setTimeout(change6, tempo);
+};
+function change6(){
+	document.getElementById("a6").style.background = "orange";
+};
+
+function area7(){
+	document.getElementById("a7").style.background = "black";
+	setTimeout(change7, tempo);
+};
+function change7(){
+	document.getElementById("a7").style.background = "orange";
+};
+
+function area8(){
+	document.getElementById("a8").style.background = "black";
+	setTimeout(change8, tempo);
+};
+function change8(){
+	document.getElementById("a8").style.background = "orange";
+};
+
+function area9(){
+	document.getElementById("a9").style.background = "black";
+	setTimeout(change9, tempo);
+};
+function change9(){
+	document.getElementById("a9").style.background = "orange";
+};
